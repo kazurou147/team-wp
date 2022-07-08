5ea1fe998ee2039c8bae20af2438a36ba206d30f
kazuhiro maeda <kazurou147@gmail.com>
2022-07-08 14:20:51 +0900

    競合対応

--- template/link-bottom.php
+++ template/link-bottom.php
@@ -1,8 +1,5 @@
-<<<<<<< HEAD <div class="l-container">
+<div class="l-container">
   <aside class="p-content-bottom-link">
 
   </aside>
-  </div>
-  =======
-  <!-- フッターで共通表示したい時に仕様 -->
-  >>>>>>> 0ba36408fe40a7d7587683e71b23a5d823ca3e0b
+</div>
